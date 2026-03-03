d47222cf4505bcd4ff074f8f9b767617b127ce16
prashant272 <prashantkrjha12@gmail.com>
2026-03-03 15:52:08 +0530

    some changes done

--- client/src/pages/DeveloperAuth.jsx
+++ client/src/pages/DeveloperAuth.jsx
@@ -27,7 +27,8 @@ export default function DeveloperAuth() {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ password })
-            });
+            }); 
+            //hello
 
             const data = await res.json();
 
